6b7ac3c7a93c3d6a5e786eb5830b1cb5d468e6c7
Farhannuari Bani Romadhan <nuari.br@gmail.com>
2025-01-26 22:12:12 +0700

    Create migration

--- DHI_Challenges.Services/Migrations/20250126151006_Add IsDelete.Designer.cs
+++ DHI_Challenges.Services/Migrations/20250126151006_Add IsDelete.Designer.cs
@@ -4,6 +4,7 @@ using DHI_Challenges.Services.Infrastructures;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DHI_Challenges.Services.Migrations
 {
     [DbContext(typeof(DHIContexts))]
-    partial class DHIContextsModelSnapshot : ModelSnapshot
+    [Migration("20250126151006_Add IsDelete")]
+    partial class AddIsDelete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DHI_Challenges.Services/Migrations/20250126151006_Add IsDelete.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DHI_Challenges.Services.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDelete",
+                schema: "User",
+                table: "MasterUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDelete",
+                schema: "Product",
+                table: "MasterProducts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDelete",
+                schema: "Transaction",
+                table: "Headers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDelete",
+                schema: "User",
+                table: "MasterUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsDelete",
+                schema: "Product",
+                table: "MasterProducts");
+
+            migrationBuilder.DropColumn(
+                name: "IsDelete",
+                schema: "Transaction",
+                table: "Headers");
+        }
+    }
+}

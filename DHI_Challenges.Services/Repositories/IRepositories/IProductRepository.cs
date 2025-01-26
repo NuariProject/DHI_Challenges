e2a1e7f9d298964170e62436b816cc8d8f63f005
Farhannuari Bani Romadhan <nuari.br@gmail.com>
2025-01-27 06:45:46 +0700

    Create Product Repository

---
+++ DHI_Challenges.Services/Repositories/IRepositories/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using DHI_Challenges.Models.Entities;
+
+namespace DHI_Challenges.Services.Repositories.IRepositories
+{
+    public interface IProductRepository : IGenericRepository<MasterProduct>
+    {
+        void Update(MasterProduct product);
+    }
+}
---
+++ DHI_Challenges.Services/Repositories/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using DHI_Challenges.Models.Entities;
+using DHI_Challenges.Services.Infrastructures;
+using DHI_Challenges.Services.Repositories.IRepositories;
+
+namespace DHI_Challenges.Services.Repositories
+{
+    public class ProductRepository : GenericRepository<MasterProduct>, IProductRepository
+    {
+        private DHIContexts _context;
+
+        public ProductRepository(DHIContexts contexts) : base(contexts)
+        {
+            _context = contexts;
+        }
+        public void Update(MasterProduct product)
+        {
+            _context.Update(product);
+        }
+    }
+}

433b86f96dd36d6610ae82b0a3cd1390e7f6d40d
Farhannuari Bani Romadhan <nuari.br@gmail.com>
2025-01-26 21:32:04 +0700

    Add constanta

---
+++ DHI_Challenges.Services/Utils/ConstField.cs
@@ -0,0 +1,10 @@
+﻿namespace DHI_Challenges.Services.Utils
+{
+    public static class Message
+    {
+        public const string SUCCESS = "Success";
+        public const string DATA_NOT_EXIST = "Data not exist";
+        public const string DATA_NOT_FOUND = "Data not found";
+        public const string VALIDATION_ERROR = "One or more validation errors";
+    }
+}

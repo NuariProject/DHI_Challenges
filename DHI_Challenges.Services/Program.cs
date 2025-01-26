416c44d575e594e6d980014f7005a3416196fcde
Farhannuari Bani Romadhan <nuari.br@gmail.com>
2025-01-26 21:10:03 +0700

    Make custom response model state invalid

--- DHI_Challenges.Services/Program.cs
+++ DHI_Challenges.Services/Program.cs
@@ -1,7 +1,9 @@
+using DHI_Challenges.Models.DataTransferObject;
 using DHI_Challenges.Services.Infrastructures;
 using DHI_Challenges.Services.MiddleWares;
 using DHI_Challenges.Services.Repositories;
 using DHI_Challenges.Services.Repositories.IRepositories;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,8 +13,32 @@ builder.Services.AddDbContext<DHIContexts>(option => option.UseSqlServer(builder
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-
 builder.Services.AddControllers();
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
+    {
+        var errors = context.ModelState
+            .Where(ms => ms.Value.Errors.Count > 0)
+            .Select(ms => new ValidationErrorsDto
+            {
+                Field = ms.Key,
+                Message = ms.Value.Errors.First().ErrorMessage
+            })
+            .ToList();
+
+        ResponseErrorDto response = new()
+        {
+            StatusCode = StatusCodes.Status400BadRequest,
+            Message = "Validation failed",
+            Errors = errors
+        };
+
+        return new BadRequestObjectResult(response);
+    };
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

6c0a8b280c54d9caecd6057e548a4df156e2fe67
Farhannuari Bani Romadhan <nuari.br@gmail.com>
2025-01-27 06:44:13 +0700

    Add Product dto

---
+++ DHI_Challenges.Models/DataTransferObject/RequestProductDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DHI_Challenges.Models.DataTransferObject
+{
+    public class RequestProductDto
+    {
+        [Required(ErrorMessage = "ProductName Can't Empty")]
+        [StringLength(25, MinimumLength = 1, ErrorMessage = "ProductName Maximal 25 Character")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Price can't be empty")]
+        [Range(1, double.MaxValue, ErrorMessage = "Price must be greater than zero")]
+        public decimal Price { get; set; }
+    }
+}

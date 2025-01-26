ce8b879369dc91537e50d6c3b97c2ccbae5483e2
Farhannuari Bani Romadhan <nuari.br@gmail.com>
2025-01-26 20:26:00 +0700

    Add Middleware for Handler Exception

---
+++ DHI_Challenges.Services/MiddleWares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using DHI_Challenges.Models.DataTransferObject;
+
+namespace DHI_Challenges.Services.MiddleWares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string tracerId = context.Items["TracerId"]?.ToString() ?? Guid.NewGuid().ToString();
+
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                _logger.LogError($"TracerId: {tracerId}, Error: {ex.Message}");
+
+                // Return a standardized error response
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                var response = new ResponseExceptionDto() 
+                {
+                    StatusCode =  StatusCodes.Status500InternalServerError,
+                    Message = ex.Message, //"An unexpected error occurred."
+                    TracerId = tracerId
+                };
+
+                await context.Response.WriteAsync(System.Text.Json.JsonSerializer.Serialize(response));
+            }
+        }
+    }
+}
--- DHI_Challenges.Services/Program.cs
+++ DHI_Challenges.Services/Program.cs
@@ -1,10 +1,15 @@
 using DHI_Challenges.Services.Infrastructures;
+using DHI_Challenges.Services.MiddleWares;
+using DHI_Challenges.Services.Repositories;
+using DHI_Challenges.Services.Repositories.IRepositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<DHIContexts>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("dhi_connect")));
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 
 builder.Services.AddControllers();
@@ -20,6 +25,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.UseHttpsRedirection();
 

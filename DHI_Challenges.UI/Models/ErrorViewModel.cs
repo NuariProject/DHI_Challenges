40ca9e44cc0c36ec1df8f2ef2ff001e03fc09703
Farhannuari Bani Romadhan <nuari.br@gmail.com>
2025-01-26 16:47:51 +0700

    Add UI Layer

---
+++ DHI_Challenges.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DHI_Challenges.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

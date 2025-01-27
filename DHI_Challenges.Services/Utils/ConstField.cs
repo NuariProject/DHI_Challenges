b56c38ff35e7c1af9a2b90694c1cb1fb6674e884
Farhannuari Bani Romadhan <nuari.br@gmail.com>
2025-01-27 10:12:35 +0700

    Update Const

--- DHI_Challenges.Services/Utils/ConstField.cs
+++ DHI_Challenges.Services/Utils/ConstField.cs
@@ -3,12 +3,14 @@
     public static class Message
     {
         public const string SUCCESS = "Success";
-        public const string DATA_NOT_EXIST = "Data not exist";
+        public const string DATA_NOT_EXIST = "Data does not exist";
+        public const string DATA_SOME_NOT_EXIST = "Some data do not exist";
         public const string DATA_NOT_FOUND = "Data not found";
-        public const string VALIDATION_ERROR = "One or more validation errors";
+        public const string DATA_SOME_PRODUCT_NOT_FOUND = "Some product not found";
+        public const string VALIDATION_ERROR = "One or more validation errors occurred";
 
         public const string TEXT_OA_DATA = "Detail Data";
         public const string TEXT_OA_ERROR = "Detail Errors";
-        public const string TEXT_OA_TRACER = "Specific tracer id";
+        public const string TEXT_OA_TRACER = "Specific tracer ID";
     }
 }

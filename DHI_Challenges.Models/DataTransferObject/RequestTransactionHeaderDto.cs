276d9f159a810315047a5e502b9d7760a3cec0a0
Farhannuari Bani Romadhan <nuari.br@gmail.com>
2025-01-27 10:16:53 +0700

    Add Response and Request Transaction dto

---
+++ DHI_Challenges.Models/DataTransferObject/RequestTransactionHeaderDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace DHI_Challenges.Models.DataTransferObject
+{
+    public class RequestTransactionHeaderDto<T>
+    {
+        [Required(ErrorMessage = "UserID can't be empty")]
+        [JsonPropertyOrder(2)]
+        public int UserID { get; set; }
+
+        [JsonPropertyOrder(5)]
+        public List<T> Details { get; set; }
+    }
+
+    public class RequestTransactionDetailDto
+    {
+        [Required(ErrorMessage = "ProductID can't be empty")]
+        [JsonPropertyOrder(1)]
+        public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Quantity can't be empty")]
+        [Range(1, 100, ErrorMessage = "Quantity must be in the range 1 to 100")]
+        [JsonPropertyOrder(2)]
+        public int Quantity { get; set; }
+    }
+}
---
+++ DHI_Challenges.Models/DataTransferObject/ResponseTransactionHeaderDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace DHI_Challenges.Models.DataTransferObject
+{
+    public class ResponseTransactionHeaderDto : RequestTransactionHeaderDto<ResponseransactionDetailDto>
+    {
+        [JsonPropertyOrder(1)]
+        public int HeaderID { get; set; }
+
+        [JsonPropertyOrder(3)]
+        public int SummaryQuantity { get; set; }
+
+        [JsonPropertyOrder(4)]
+        public DateTime CreateDate { get; set; }
+    }
+    public class ResponseransactionDetailDto : RequestTransactionDetailDto
+    {
+        [JsonPropertyOrder(3)]
+        public decimal Price { get; set; }
+    }
+}
